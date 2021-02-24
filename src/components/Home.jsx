00b6392023bade0dc91f758383800537f6f07b23
Hunain <muhammadhunain072@gmail.com>
2021-02-24 19:29:31 +0500

    First commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+
+import React from "react";
+import './Home.css';
+import Directory from './directory';
+const Home = () => (
+    <div className='homepage'>
+        <Directory/>
+    </div>
+);
+export default Home;
